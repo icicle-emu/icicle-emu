f6a7c737244b10b46124931cc74b50d4728431cb
mchesser <michael.chesser@adelaide.edu.au>
2025-10-07 14:07:33 +1030

    Fix no fuel being consumed when stepping from the middle of a block

--- icicle-vm/src/tests.rs
+++ icicle-vm/src/tests.rs
@@ -1,6 +1,6 @@
 use icicle_cpu::{
-    mem::{perm, Mapping},
     Config, ExceptionCode, VmExit,
+    mem::{Mapping, perm},
 };
 
 #[test]
@@ -54,6 +54,7 @@ fn prioritize_instruction_limit_over_decode_error() {
 
     vm.cpu.write_pc(0x1000);
     assert_eq!(vm.step(2), VmExit::InstructionLimit);
+    assert_eq!(vm.step(1), VmExit::UnhandledException((ExceptionCode::InvalidInstruction, 0x1008)));
 }
 
 #[test]
@@ -71,4 +72,33 @@ fn execute_only_memory() {
 
     vm.cpu.write_pc(0x1000);
     assert_eq!(vm.step(1), VmExit::InstructionLimit);
+}
+
+#[test]
+fn single_step_after_fault() {
+    let mut vm = crate::build(&Config::from_target_triple("i686-none")).unwrap();
+    vm.cpu.mem.map_memory_len(0, 0x100, Mapping { perm: perm::READ | perm::EXEC, value: 0 });
+
+    let reg_eax = vm.cpu.arch.sleigh.get_varnode("EAX").unwrap();
+
+    static CODE: &[u8] = &[
+        0xA1, 0xFF, 0xFF, 0x01, 0x00, // 0x00: mov eax, [0x1FFFF]
+        0xB8, 0x02, 0x00, 0x00, 0x00, // 0x05: mov eax, 2
+        0x90, // 0x0A: nop
+    ];
+    vm.cpu.mem.write_bytes(0x0, CODE, perm::NONE).unwrap();
+
+    vm.cpu.write_pc(0x00);
+    assert_eq!(vm.step(1), VmExit::UnhandledException((ExceptionCode::ReadUnmapped, 0x1FFFF)));
+    assert_eq!(vm.cpu.read_pc(), 0x00);
+
+    // Map the missing memory and single step again.
+    assert!(vm.cpu.mem.map_memory_len(0x1FF00, 0x200, Mapping { perm: perm::READ, value: 0xaa }));
+    vm.cpu.exception.clear();
+    assert_eq!(vm.step(1), VmExit::InstructionLimit);
+    assert_eq!(vm.cpu.read_reg(reg_eax), 0xAAAA_AAAA);
+    assert_eq!(vm.cpu.read_pc(), 0x05);
+
+    assert_eq!(vm.step(1), VmExit::InstructionLimit);
+    assert_eq!(vm.cpu.read_pc(), 0x0A);
 }
